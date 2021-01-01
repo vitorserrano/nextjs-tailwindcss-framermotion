9d393ae05422ed80e41e61f40b013619d5b544fe
vitorserrano <vitorcirinoserrano@gmail.com>
2021-01-01 19:10:11 -0300

    💅 feat: Add home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,19 @@ import Head from 'next/head';
 
 import RocketseatLogo from '../assets/rocketseat.svg';
 
+import { Container } from '../styles/pages/Home';
+
 const Home: React.FC = () => {
   return (
-    <div>
+    <Container>
       <Head>
         <title>ReactSetup</title>
       </Head>
 
-      <main>
-        <RocketseatLogo />
-        <h1>Hello World</h1>
-      </main>
-    </div>
+      <RocketseatLogo />
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + Next.js structure made by Rocketseat.</p>
+    </Container>
   );
 };
 
